c8d678e3192a4b7e4d645943dfffd7c1d62ced87
Johan Martinsson <jma@notsodark.com>
2023-03-05 18:34:49 +0100

    improve logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,9 +1,14 @@
 export class Logger {
     log(...msgs: string[]) {
-        console.log(new Date().toISOString(), ...msgs)
+        console.log(this.getTimeStamp(), ...msgs)
     }
 
     error(...msgs: (string | Error)[]) {
-        console.error(new Date().toISOString(), ...msgs)
+        console.error(this.getTimeStamp(), ...msgs)
+    }
+
+    private getTimeStamp() {
+        // 2023-03-05T17:17:01.421Z => 2023-03-05 17:17:01
+        return new Date().toISOString().replace("T", " ").split(".")[0]
     }
 }

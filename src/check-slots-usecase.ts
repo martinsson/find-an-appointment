3163ac64a3b2de87fff4df8f8ffd7344eca5a307
Johan Martinsson <jma@notsodark.com>
2023-03-05 18:28:40 +0100

    refactor to extract pure logic free of side-effects

--- src/check-slots-usecase.ts
+++ src/check-slots-usecase.ts
@@ -4,23 +4,44 @@ import {findDatesBefore, printDates, RdvDate} from "./dates/rdv.date";
 import {Logger} from "./logger";
 
 
+class DatesAvailable {
+    constructor(public text: string, public datesString: string) {
+
+    }
+
+}
+
+class NoDatesAvailable {
+    constructor(public message: string, public availableDates: RdvDate[]) {
+
+    }
+
+}
+
+function checkSlotsUseCaseLogic(beforeDate: RdvDate, availableDates: RdvDate[]) {
+    let datesBefore = findDatesBefore(beforeDate, availableDates);
+    if (datesBefore.length > 0) {
+        let datesString = printDates(datesBefore);
+        let text = `dates disponibles: \n${datesString}`;
+        return new DatesAvailable(text, datesString)
+    } else {
+        if (availableDates.length === 0) {
+            return new NoDatesAvailable("no dates available at all", availableDates)
+        } else {
+            return new NoDatesAvailable("no dates available, first date is " + availableDates[0], availableDates)
+        }
+    }
+}
+
 export async function checkSlotsUseCase(examenCode: string, beforeDate: RdvDate) {
     const logger = new Logger()
     const searchResponse = await invokeCliniqueMailSearch(examenCode)
     const availableDates = extractAvailableDates(searchResponse.data)
-    let datesBefore = findDatesBefore(beforeDate, availableDates);
-    if (datesBefore.length > 0) {
-        let datesString = printDates(datesBefore);
-        logger.log("found dates before : \n", datesString)
-        let text = `dates disponibles: \n${datesString}`;
-        await axios.post("https://hooks.slack.com/services/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", {text});
+    let result = checkSlotsUseCaseLogic(beforeDate, availableDates);
+    if (result instanceof DatesAvailable) {
+        logger.log(result.text, "notifying...")
+        await axios.post("https://hooks.slack.com/services/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", {text: result.text});
     } else {
-        if (availableDates.length === 0) {
-            logger.log("no dates available ");
-
-        } else {
-            logger.log("no dates available, first date is " + availableDates[0])
-        }
+        logger.log(result.message)
     }
-
 }
